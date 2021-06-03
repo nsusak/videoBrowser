bf028ccf19f9829ce067b224afbc4e6e3e9f738a
Nikolina Šušak <nsusak0@gmail.com>
2021-06-03 13:38:12 +0200

    fixing some issues and warnings, default setup

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,20 @@ import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null};
+    componentDidMount(){
+        //Default search term while the page loads
+        this.onTermSubmit("javascript")
+    }
     onTermSubmit = async term => {
         const response = await youtube.get("/search", {
             params: {
                 q: term
             }
         });
-        this.setState({ videos: response.data.items });
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+            });
     };
     onVideoSelect = (video) => {
         this.setState({selectedVideo:video});
@@ -21,13 +28,25 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList 
-                onVideoSelect={this.onVideoSelect} 
-                videos={this.state.videos} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList 
+                                onVideoSelect={this.onVideoSelect} 
+                                videos={this.state.videos} 
+                            />
+                        </div>
+                    </div>        
+                </div>
             </div>
         );
     }
 }
+ 
+ 
+
 
 export default App;
